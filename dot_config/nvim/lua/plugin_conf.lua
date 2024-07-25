1d6d9c4a7a6a9a9f80e397383ee9cba1e438fb34
Aaron Carson <aaron@aaroncarson.co.uk>
2024-07-25 12:49:07 +0100

    Remove view.mappings

--- dot_config/nvim/lua/plugin_conf.lua
+++ dot_config/nvim/lua/plugin_conf.lua
@@ -1,13 +1,10 @@
 -- Set up nvim-tree
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
+  sort = {
+    sorter = "case_sensitive",
+  },
   view = {
     width = 30,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
   },
   renderer = {
     group_empty = true,
